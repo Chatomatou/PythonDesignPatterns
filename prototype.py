ed4f0b1fea0951f821c67ca59c5de2bd4153948a
Naoki <jason.champagne@laposte.net>
2018-02-17 15:55:48 +0100

    Ajout prototype.py

---
+++ prototype.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+
+import copy
+
+class Prototype:
+    pass
+
+#---------------------------------------------
+def main():
+    original = Prototype()
+    prototype = copy.deepcopy(original)
+
+if __name__ == "__main__":
+    main()
