fd518b9a7b8475225cbf54ba052f654f4e655143
Naoki <jason.champagne@laposte.net>
2018-02-18 19:39:26 +0100

    Ajout state.py

---
+++ state.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+import abc
+
+class Context:
+    def __init__(self, state):
+        self._state = state
+
+    def request(self):
+        self._state.handle()
+
+class State(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def handle(self):
+        pass
+
+class ConcreteState1(State):
+    def handle(self):
+        pass
+
+class ConcreteState2(State):
+    def handle(self):
+        pass
+
+def main():
+    cs1 = ConcreteState1()
+    context = Context(cs1)
+    context.request()
+
+if __name__ == "__main__":
+    main()
